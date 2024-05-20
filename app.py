5cf704a9532e0931626548d7974a014e3fa58e51
William Chen <wachen@ucdavis.edu>
2024-05-20 14:47:03 -0700

    Created basic dynamic usage

--- app.py
+++ app.py
@@ -1,61 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, json, redirect
+from flask_mysqldb import MySQL
+from flask import request
 import os
-
-# Configuration
 
 app = Flask(__name__)
 
-# Example Data
+app.config["MYSQL_HOST"] = "classmysql.engr.oregonstate.edu"
+app.config["MYSQL_USER"] = "cs340_chenwill"
+app.config["MYSQL_PASSWORD"] = "JeuYwPY70IXx"
+app.config["MYSQL_DB"] = "cs340_chenwill"
+app.config["MYSQL_CURSORCLASS"] = "DictCursor"
 
-customers_from_app_py = [
-{
-    "FirstName" : "Patrick",
-    "LastName" : "Kim",
-    "Email" : "kimpatr@oregonstate.edu"
-},{
-    "FirstName" : "William",
-    "LastName" : "Chen",
-    "Email" : "chenwill@oregonstate.edu"
-},{
-    "FirstName" : "Jane",
-    "LastName" : "Smith",
-    "Email" : "jsmith@mail.com"
-}
- ]
 
-orders_from_app_py = [
-    {
-        "Date" : "2024-03-23",
-        "CustomerID" : 1
-    },
-    {
-        "Date" : "2024-03-31",
-        "CustomerID" : 2
-    },
-    {
-        "Date" : "2024-04-02",
-        "CustomerID" : 1
-    },
-    {
-        "Date" : "2024-04-20",
-        "CustomerID" : 3
-    }
-]
+mysql = MySQL(app)
+
+
 # Routes
 @app.route('/')
 def root():
     return render_template("main.j2")
 
-@app.route('/customers')
-def customers():
-    return render_template("customers.j2", customers = customers_from_app_py)
+@app.route('/orderdetails')
+def orderdetails():
+    query = "SELECT * FROM orderdetails;"
+    cursor = db.execute_query(db_connection=db_connection, query=query)
+    results = cursor.fetchall()
+    return render_template("orderdetails.j2", order_details_results = results)
 
-@app.route('/orders')
-def orders():
-    return render_template("orders.j2", orders = orders_from_app_py)
+# Listener
+if __name__ == "__main__":
 
-#debug = True will automatically refresh app so we don't have to keep rerun script
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8000))
-    
-    app.run(debug=True, port=port)
+    #Start the app on port 3000, it will be different once hosted
+    app.run(port = 9001, debug = True)
