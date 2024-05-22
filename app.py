fb88f97acfb8709f06df19213243e8b1ecb7723e
William Chen <wachen@ucdavis.edu>
2024-05-22 11:20:15 -0700

    Final Commits, non-working MySQL

--- app.py
+++ app.py
@@ -1,60 +1,92 @@
-from flask import Flask, render_template, json, json, redirect
-from flask_mysqldb import MySQL
-from flask import request
-import os
-import Step4.database.db_connector as db
 
-# Configuration
+from flask import Flask, render_template, json, request, redirect
+from flask_mysqldb import MySQL
+import os
+import database.db_connector as datab
 
 app = Flask(__name__)
-db_connection = db.connect_to_database()
-
-app.config["MYSQL_HOST"] = "classmysql.engr.oregonstate.edu"
-app.config["MYSQL_USER"] = "cs340_chenwill"
-app.config["MYSQL_PASSWORD"] = "JeuYwPY70IXx"
-app.config["MYSQL_DB"] = "cs340_chenwill"
-app.config["MYSQL_CURSORCLASS"] = "DictCursor"
-
-
+host = os.environ.get("340DBHOST")
+user = os.environ.get("340DBUSER")
+passwd = os.environ.get("340DBPW")
+db = os.environ.get("340DB")
 mysql = MySQL(app)
 
+db_connection = datab.connect_to_database(host, user, passwd, db)
 
-# Routes
+# Routes 
+
 @app.route('/')
 def root():
     return render_template("main.j2")
 
-@app.route('/order-Details')
-def order_details():
-    query = "SELECT * FROM OrderDetails;"
-    cursor = db.execute_query(db_connection=db_connection, query=query)
+@app.route('/bsg-people')
+def bsg_people():
+    query = "SELECT * FROM bsg_people;"
+    cursor = datab.execute_query(db_connection=db_connection, query=query)
     results = cursor.fetchall()
-    return render_template("orderDetails.j2", OrderDetails=results)
-    # return results
+    return render_template("bsg.j2", bsg_people=results)
 
-@app.route('/createorderdetails')
-def create_order_detail():
-    if not BookISBN and not OID:
-        return redirect("/orderdetails")
-    if not BookISBN:
-        query = "INSERT INTO OrderDetails (OID, OrderQty, UnitPrice, LineTotal) VALUES (%s, 0, 0, 0)"
-        cur = mysql.connection.cursor()
-        cur.execute(query, (OID, OrderQty, UnitPrice, LineTotal))
-        mysql.connection.commit()
-    elif not OID:
-        query = "INSERT INTO OrderDetails (BookISBN, OrderQty, UnitPrice, LineTotal) VALUES (%s, %s, (SELECT Price FROM Books WHERE BookISBN = BookISBN), UnitPrice * LineTotal)"
-        cur = mysql.connection.cursor()
-        cur.execute(query, (OID, OrderQty, UnitPrice, LineTotal))
-        mysql.connection.commit()  
-    else:
-        query = "INSERT INTO OrderDetails (BookISBN, OID, OrderQty, UnitPrice, LineTotal) VALUES (%s, %s, %s, (SELECT Price FROM Books WHERE BookISBN = BookISBN), UnitPrice * LineTotal)"
-        cur = mysql.connection.cursor()
-        cur.execute(query, (OID, OrderQty, UnitPrice, LineTotal))
-        mysql.connection.commit()
-    return redirect("/orderdetails")
+@app.route('/orderdetails', methods=["POST", "GET"])
+def orderdetails():
+    if request.method == "GET":
+        query = "SELECT OrderDetails.ID, Books.ISBN, Books.Title, OrderDetails.OID, OrderDetails.OrderQty AS `Order Quantity`, OrderDetails.UnitPrice AS `Unit Price`, (OrderDetails.UnitPrice * OrderDetails.OrderQty) AS LineTotal FROM OrderDetails INNER JOIN Books ON OrderDetails.BookISBN = Books.ISBN ORDER BY OrderDetails.OID, Books.ISBN;"
+        cursor = datab.execute_query(db_connection=db_connection, query=query)
+        data = cursor.fetchall()
+        
+        # mySQL query to grab book ID/Title data for dropdown
+        query2 = "SELECT ISBN, Title FROM Books"
+        cursor = datab.execute_query(db_connection=db_connection, query=query2)
+        cursor.execute(query2)
+        title_data = cursor.fetchall()
+        
+        return render_template("orderDetails.j2", data=data, titles=title_data)
 
+@app.route('/create-order-details', methods=["POST", "GET"])
+def create_order():
+    if request.method == "POST":
+        ISBN = request.form["BookISBN"]
+        OID = request.form["OrderID"]
+        OrderQty = request.form["OrderQty"]
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 3434))
+        if not OID and not ISBN:
+            return redirect('/orderdetails')
+        
+        if not ISBN:
+            query = f"INSERT INTO OrderDetails (BookISBN, OID, OrderQty, UnitPrice, LineTotal) VALUES (NULL, {OID}, {OrderQty}, 0, 0);"
+        elif not OID:
+            query = f"INSERT INTO OrderDetails (BookISBN, OID, OrderQty, UnitPrice, LineTotal) VALUES ({ISBN}, NULL, {OrderQty}, (SELECT Price FROM Books WHERE BookISBN = {ISBN}), (SELECT Price FROM Books WHERE BookISBN = {ISBN}) * OrderQty);"
+        else:
+            query = f"INSERT INTO OrderDetails (BookISBN, OID, OrderQty, UnitPrice, LineTotal) VALUES ({ISBN}, {OID}, {OrderQty}, (SELECT Price FROM Books WHERE BookISBN = {ISBN}), (SELECT Price FROM Books WHERE BookISBN = {ISBN}) * {OrderQty});"
+
+        cursor = datab.execute_query(db_connection=db_connection, query=query)
+        cursor.execute(query)
+
+        return redirect('/orderdetails')
     
-    app.run(debug=True, port=port)
+@app.route('/delete-order-details/<int:id>')
+def delete_order_details(id):
+    query = f"DELETE FROM OrderDetails WHERE ID = {id}"
+    cur = mysql.connection.cursor()
+    cur.execute(query)
+    mysql.connection.commit()
+
+    return redirect('/orderdetails')
+        
+        
+    
+@app.route('/orders', methods=["GET"])
+def orders():
+    query = "SELECT * FROM Orders"
+    cur = mysql.connection.cursor()
+    cur.execute(query)
+    data = cur.fetchall()
+    
+    # render edit_orderDetails page
+    return render_template("orders.j2", data=data)
+
+# Listener
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 9112))
+    print(host, user, passwd, db)
+    app.run(port=port, debug=True)
