4a84bdfb46972fc06af04a59c2b2425226b0eadf
William Chen <wachen@ucdavis.edu>
2024-05-30 10:23:30 -0700

    Added Step 5 and Customer -> CID in app.py

---
+++ Step5/DML.sql
@@ -0,0 +1,101 @@
+-- Patrick Kim, William Chen
+-- CS340
+-- Date: 05/06/24
+
+---------------------------------------------------------
+-- CUSTOMERS
+-- Get All customers for Browse Customer Page
+SELECT FirstName AS First Name, LastName AS Last Name, Email AS E-mail FROM Customers;
+
+-- Add new Customer
+INSERT INTO Customers (FirstName, LastName, Email)
+VALUES (:FirstNameInput, :LastNameInput, :EmailInput);
+
+-- Delete A Customer
+DELETE FROM Customers WHERE ID = :CustomerIDfromDropDown;
+
+-- Update a Customer
+-- No update to CID at the moment
+-- Update based on ID
+UPDATE Customers
+SET FirstName="Foo", LastName="Bar", Email="Foobar@gmail.com"
+WHERE ID = :CustomerIDfromDropDown;
+
+---------------------------------------------------------
+--ORDERS
+-- Get All Orders and the customer name  For Browse Orders Page
+SELECT Orders.ID AS OrderID, Date, CID as Customer ID
+FROM Orders
+    INNER JOIN Customers
+    ON CID = Customers.ID;
+
+-- Add New Order
+INSERT INTO Orders (Date, CID)
+VALUES (:DateInput, :CustomerIDfromDropDown)
+
+---------------------------------------------------------
+--ORDER DETAILS
+-- Show All OrderDetails and show Order Id, Book Title, and Unit Price
+SELECT Books.ISBN, OrderDetails.OID, OrderDetails.OrderQty AS `Order Quantity`, OrderDetails.UnitPrice AS `Unit Price`, (OrderDetails.UnitPrice * OrderDetails.OrderQty) AS LineTotal
+    FROM OrderDetails
+        INNER JOIN Books ON OrderDetails.BookISBN = Books.ISBN
+        ORDER BY OrderDetails.OID, Books.ISBN;
+
+-- Show Invoice Slip Data (Filtered by Order, Currently showing OID = 1)
+-- Header 
+SELECT Orders.ID, CONCAT(Customers.FirstName, ' ', Customers.LastName) AS FullName, CURDATE() AS CurrentDate
+    FROM Customers
+        INNER JOIN Orders ON Customers.ID = Orders.CID
+        WHERE Orders.ID = 1;
+-- Body
+SELECT Books.ISBN, Books.Title, OrderDetails.LineTotal, OrderDetails.OrderQty, OrderDetails.UnitPrice
+    FROM OrderDetails
+        INNER JOIN Books ON OrderDetails.BookISBN = Books.ISBN
+        WHERE OrderDetails.OID = 1;
+
+-- Associate a Book with an Order (M:M addition)
+INSERT INTO OrderDetails (BookISBN, OID, OrderQty, UnitPrice, LineTotal)
+    VALUES (:book_ISBN_selected_from_Order_details_list, :order_ID_selected_from_Order_details_list, :OrderQtyInput, 
+    (SELECT Price AS Uprice FROM Books WHERE :book_ISBN_selected_from_Order_details_list = ISBN) , :OrderQtyIpnut*Uprice)
+
+-- Update Order Details
+-- Subquery to retrieve book ID and Order ID
+UPDATE OrderDetails
+SET BookISBN=(SELECT ISBN FROM Books WHERE ID=2), OID=(SELECT ID FROM Orders WHERE ID=1), OrderQty=2, UnitPrice=2, LineTotal=OrderQty*UnitPrice
+WHERE ID=1;
+
+-- Dissassociate an Book from an Order (M:M deletion)
+DELETE FROM OrderDetails
+WHERE BookISBN = :book_ISBN_selected_from_Order_details_list AND OID = :order_ID_selected_from_Order_details_list;
+
+---------------------------------------------------------
+-- AUTHORS
+-- Getting Authors
+SELECT ID AS Author ID, FirstName as First Name, LastName as Last Name, Gender FROM Authors;
+
+-- Create Author
+INSERT INTO Authors (FirstName, LastName, Gender)
+VALUES (:FirstNameInput, :LastNameInput, :GenderInput);
+
+---------------------------------------------------------
+-- BOOKS
+-- Getting Books
+-- Example of grabbing ISBN 1
+SELECT Books.ISBN AS ISBN, Books.Title AS Title, Books.Genre AS Genre, Books.Stock AS Stock, Books.Price AS Price, CONCAT(Authors.FirstName, ' ', Authors.LastName) AS Author, Publishers.Company AS Publisher
+    FROM Books
+        INNER JOIN Authors ON Authors.ID = Books.AID
+        INNER JOIN Publishers ON Publishers.ID = Books.PID
+    WHERE Books.ISBN = 1;
+
+-- Create Book
+INSERT INTO Books (Title, Genre, Stock, Price, AID, PID)
+VALUES (:TitleInput, :GenreInput, :StockInput, :PriceInput, (SELECT ID FROM Author WHERE FirstName = "Foo" and LastName = "Bar"), (SELECT ID FROM Publishers WHERE Company = "Foo"));
+
+---------------------------------------------------------
+-- PUBLISHERS
+-- Getting Publishers
+SELECT ID AS Publisher ID, Company, Year as Year Founded  FROM Publishers;
+
+-- Create Publisher
+INSERT INTO Publishers (Company, Year)
+VALUES (:CompanyInput, :YearInput);
---
+++ Step5/README.md
@@ -0,0 +1,8 @@
+<h1>BookstoreSQL</h1>
+
+<h2>CS 340 Group Project, BookStore for William Chen and Patrick Kim.</h2>
+
+<h3>Works Cited</h3>
+<p>This project adapts code from: <a href = https://github.com/osu-cs340-ecampus/flask-starter-app?tab=readme-ov-file.>CS 340 Flask Starter</a><p>
+
+<p>The adapted code can be found in the database folder. As such, the only use of its implementation is within the app.py file. Furthermore, this code aims to provide a connection to our MySQL database to help faciliate CRUD operations on our MySQL database without having to implement a novel function to connect to said database.</p>
